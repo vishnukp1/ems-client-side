be636f121c05f5993b4332d00d51f1965a989742
wishtony <vishnutoanother@gmail.com>
2023-12-04 15:17:10 +0530

    new update

--- src/Autherization/Autherization.js
+++ src/Autherization/Autherization.js
@@ -4,7 +4,7 @@ const token = localStorage.getItem("token");
 const companyId =  localStorage.getItem("companyId")
 
 const api = axios.create({
-  baseURL: 'http://13.126.229.168:4445', 
+  baseURL: 'https://13.126.229.168:4445', 
   headers: {
     'Authorization': `Bearer ${token}`, 
     'Content-Type': 'application/json',
