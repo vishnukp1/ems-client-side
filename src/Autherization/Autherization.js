b11b98264cd327d8fecf4b27b228865d8b277061
wishtony <vishnutoanother@gmail.com>
2023-12-04 14:52:44 +0530

    new update

--- src/Autherization/Autherization.js
+++ src/Autherization/Autherization.js
@@ -4,7 +4,7 @@ const token = localStorage.getItem("token");
 const companyId =  localStorage.getItem("companyId")
 
 const api = axios.create({
-  baseURL: 'https://ems-app1.onrender.com', 
+  baseURL: 'http://13.126.229.168:4445', 
   headers: {
     'Authorization': `Bearer ${token}`, 
     'Content-Type': 'application/json',
