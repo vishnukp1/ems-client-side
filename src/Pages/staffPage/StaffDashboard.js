7ad10bdb49227b29e6118ea8a6b5f1aad076dc2b
wishtony <vishnutoanother@gmail.com>
2023-12-25 08:51:06 +0530

    new commit

--- src/Pages/staffPage/StaffDashboard.js
+++ src/Pages/staffPage/StaffDashboard.js
@@ -16,144 +16,163 @@ function StaffDashboard() {
   const navigate = useNavigate();
 
   return (
-    <div style={{display:'flex', flexDirection:'column'}}>
-    <StaffNav />
-    <div style={{display:"flex", width:"100vw",height:"100vh"}}>
-      <StaffSidebar/>
-      <div
-        className="main-container"
-        style={{ backgroundColor: "#1E1E2E", display: "flex" ,overflowY:"auto" }}
-      >
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <StaffNav />
+      <div style={{ display: "flex", width: "100vw", height: "100vh" }}>
+        <StaffSidebar />
         <div
-          className="dashboard-div"
+          className="main-container"
           style={{
+            backgroundColor: "#1E1E2E",
             display: "flex",
-            justifyContent: "space-around",
-            width: "100%",
-            flexWrap: "wrap",
+            overflowY: "auto",
           }}
         >
-  <Card
+          <div
+            className="dashboard-div"
             style={{
-              width: "20rem",
-              height: "13rem",
-              background: "#27293D",
-              color: "white",
-              //  flexWrap:'wrap'
-              marginTop:"2rem"
+              display: "flex",
+              justifyContent: "space-around",
+              width: "100%",
+              flexWrap: "wrap",
             }}
           >
-            <Card.Body>
-              <UsersThree  style={{marginTop:".1rem",height:"4rem",width:"4rem"}} size={32} />
-              <Card.Title>Profile</Card.Title>
-              <Card.Text></Card.Text>
-              <Button
-                variant="primary"
-                onClick={() => navigate(`/employees/staffprofile`)}
-                style={{marginTop:".2rem"}}
-              >
-                More Info
-              </Button>
-            </Card.Body>
-          </Card>
+            <Card
+              style={{
+                width: "20rem",
+                height: "13rem",
+                background: "#27293D",
+                color: "white",
+                //  flexWrap:'wrap'
+                marginTop: "2rem",
+              }}
+            >
+              <Card.Body>
+                <UsersThree
+                  style={{ marginTop: ".1rem", height: "4rem", width: "4rem" }}
+                  size={32}
+                />
+                <Card.Title>Profile</Card.Title>
+                <Card.Text></Card.Text>
+                <Button
+                  variant="primary"
+                  onClick={() => navigate(`/employees/staffprofile`)}
+                  style={{ marginTop: ".2rem" }}
+                >
+                  More Info
+                </Button>
+              </Card.Body>
+            </Card>
 
+            <Card
+              style={{
+                width: "20rem",
+                height: "13rem",
+                background: "#27293D",
+                color: "white",
+                //  flexWrap:'wrap'
+                marginTop: "2rem",
+              }}
+            >
+              <Card.Body>
+                <UsersThree
+                  style={{ marginTop: ".1rem", height: "4rem", width: "4rem" }}
+                  size={32}
+                />
+                <Card.Title>All Employee</Card.Title>
+                <Card.Text></Card.Text>
+                <Button
+                  variant="primary"
+                  onClick={() => navigate("/employees/viewstaff")}
+                  style={{ marginTop: ".2rem" }}
+                >
+                  More Info
+                </Button>
+              </Card.Body>
+            </Card>
 
-          <Card
-            style={{
-              width: "20rem",
-              height: "13rem",
-              background: "#27293D",
-              color: "white",
-              //  flexWrap:'wrap'
-              marginTop:"2rem"
-            }}
-          >
-            <Card.Body>
-              <UsersThree style={{marginTop:".1rem",height:"4rem",width:"4rem"}} size={32} />
-              <Card.Title>All Employee</Card.Title>
-              <Card.Text></Card.Text>
-              <Button
-                variant="primary"
-                onClick={() => navigate("/employees/viewstaff")}
-                style={{marginTop:".2rem"}}
-              >
-                More Info
-              </Button>
-            </Card.Body>
-          </Card>
-
-          <Card
-            style={{
-              width: "20rem",
-              height: "13rem",
-              background: "#27293D",
-              color: "white",
-              //  flexWrap:'wrap'
-              marginTop:"2rem"
-            }}
-          >
-            <Card.Body>
-              <PersonSimpleBike style={{marginTop:".1rem",height:"4rem",width:"4rem"}} size={32} />
-              <Card.Title>View Task</Card.Title>
-              <Card.Text></Card.Text>
-              <Button
-                variant="primary"
-                onClick={() => navigate("/employees/viewtask")}
-                style={{marginTop:".2rem"}}
-              >
-                More Info
-              </Button>
-            </Card.Body>
-          </Card>
-          <Card
-            style={{
-              width: "20rem",
-              height: "13rem",
-              background: "#27293D",
-              color: "white",
-              //  flexWrap:'wrap'
-              marginTop:"2rem"
-            }}
-          >
-            <Card.Body>
-              <HandWaving style={{marginTop:".1rem",height:"4rem",width:"4rem"}} size={32} />
-              <Card.Title>Attendance</Card.Title>
-              <Card.Text></Card.Text>
-              <Button
-                variant="primary"
-                onClick={() => navigate("/employees/viewattendance")}
-                style={{marginTop:".2rem"}}
-              >
-                More Info
-              </Button>
-            </Card.Body>
-          </Card>
-          <Card
-            style={{
-              width: "20rem",
-              height: "13rem",
-              background: "#27293D",
-              color: "white",
-              //  flexWrap:'wrap'
-              marginTop:"2rem"
-            }}
-          >
-            <Card.Body>
-              <HandWaving style={{marginTop:".1rem",height:"4rem",width:"4rem"}} size={32} />
-              <Card.Title>Apply Leave</Card.Title>
-              <Card.Text></Card.Text>
-              <Button
-                variant="primary"
-                onClick={() => navigate("/employees/addleave")}
-                style={{marginTop:".2rem"}}
-              >
-                More Info
-              </Button>
-            </Card.Body>
-          </Card>
+            <Card
+              style={{
+                width: "20rem",
+                height: "13rem",
+                background: "#27293D",
+                color: "white",
+                //  flexWrap:'wrap'
+                marginTop: "2rem",
+              }}
+            >
+              <Card.Body>
+                <PersonSimpleBike
+                  style={{ marginTop: ".1rem", height: "4rem", width: "4rem" }}
+                  size={32}
+                />
+                <Card.Title>View Task</Card.Title>
+                <Card.Text></Card.Text>
+                <Button
+                  variant="primary"
+                  onClick={() => navigate("/employees/viewtask")}
+                  style={{ marginTop: ".2rem" }}
+                >
+                  More Info
+                </Button>
+              </Card.Body>
+            </Card>
+            <Card
+              style={{
+                width: "20rem",
+                height: "13rem",
+                background: "#27293D",
+                color: "white",
+                //  flexWrap:'wrap'
+                marginTop: "2rem",
+              }}
+            >
+              <Card.Body>
+                <HandWaving
+                  style={{ marginTop: ".1rem", height: "4rem", width: "4rem" }}
+                  size={32}
+                />
+                <Card.Title>Attendance</Card.Title>
+                <Card.Text></Card.Text>
+                <Button
+                  variant="primary"
+                  onClick={() => navigate("/employees/viewattendance")}
+                  style={{ marginTop: ".2rem" }}
+                >
+                  More Info
+                </Button>
+              </Card.Body>
+            </Card>
+            <Card
+              style={{
+                width: "20rem",
+                height: "13rem",
+                background: "#27293D",
+                color: "white",
+                //  flexWrap:'wrap'
+                marginTop: "2rem",
+              }}
+            >
+              <Card.Body>
+                <HandWaving
+                  style={{ marginTop: ".1rem", height: "4rem", width: "4rem" }}
+                  size={32}
+                />
+                <Card.Title>Apply Leave</Card.Title>
+                <Card.Text></Card.Text>
+                <Button
+                  variant="primary"
+                  onClick={() => navigate("/employees/addleave")}
+                  style={{ marginTop: ".2rem" }}
+                >
+                  More Info
+                </Button>
+              </Card.Body>
+            </Card>
+          </div>
         </div>
       </div>
-  </div></div>
+    </div>
   );
 }
 
